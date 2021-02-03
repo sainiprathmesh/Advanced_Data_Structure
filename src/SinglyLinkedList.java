f533ec446da19555c7862a16535d248943dc0ba1
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2021-02-03 10:51:51 +0530

    03/02/2021

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -10,17 +10,28 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
-    // Generic Type
 
-    /*
-     * //head is instance variable
-     * head = new ListNode(10);
-     * ListNode second = new ListNode(1);
-     * ListNode third = new ListNode(8);
-     * ListNode fourth = new ListNode(11);
-     * head.next = second;
-     * second.next = third;
-     * third.next = fourth;
-     *
-     * */
+    // Generic Type
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+        sll.head.next = second;
+        second.next = third;
+        third.next = fourth;
+        sll.display();
+    }
+
+    public void display() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    //head is instance variable
 }
+
