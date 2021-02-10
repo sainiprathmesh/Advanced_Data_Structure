84634201d1e7056c6b718bd8d5b166f2fbc660b6
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2021-02-10 13:49:17 +0530

    10 Feb 2021

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -14,15 +14,26 @@ public class SinglyLinkedList {
     // Generic Type
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
-        sll.head = new ListNode(10);
+        /*sll.head = new ListNode(10);
         ListNode second = new ListNode(1);
         ListNode third = new ListNode(8);
         ListNode fourth = new ListNode(11);
         sll.head.next = second;
         second.next = third;
-        third.next = fourth;
+        third.next = fourth;*/
+
+        /*sll.insertFirst(11);
+        sll.insertFirst(8);
+        sll.insertFirst(1);*/
+
+        sll.insertLast(11);
+        sll.insertLast(8);
+        sll.insertLast(1);
+        sll.insertLast(10);
         sll.display();
-        System.out.println("Length of List is : " + sll.length());
+        // System.out.println("Length of List is : " + sll.length());
+        System.out.println(sll.deleteFirst().data);
+        System.out.println(sll.deleteFirst().data);
     }
 
     public void display() {
@@ -45,6 +56,35 @@ public class SinglyLinkedList {
             current = current.next;
         }
         return count;
+    }
+
+    public void insertFirst(int value) {
+        ListNode newNode = new ListNode(value);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void insertLast(int value) {
+        ListNode newNode = new ListNode(value);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        ListNode current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public ListNode deleteFirst() {
+        if (head == null) {
+            return null;
+        }
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+        return temp;
     }
     //head is instance variable
 }
